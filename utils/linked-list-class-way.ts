d8cbedaf918487f68fa3899ae0ab3788a10fa254
Neeraj Singh <869neerajsingh@gmail.com>
2025-03-05 13:14:55 +0530

    refactor: Replace anonymous functions with named method for memory optimizations & remove public keyword as it is applied by default

--- utils/linked-list-class-way.ts
+++ utils/linked-list-class-way.ts
@@ -1,6 +1,6 @@
 class ListNode {
-  public val: number;
-  public next: ListNode | null;
+  val: number;
+  next: ListNode | null;
 
   constructor(val: number, next = null) {
     this.val = val;
@@ -9,56 +9,56 @@ class ListNode {
 }
 
 class LinkedList {
-  public head: ListNode | null;
+  head: ListNode | null;
 
   constructor() {
     this.head = null;
   }
 
-  // Method to insert linked list node at head
-  private insertNodeAtHead = function (node: ListNode) {
+  // Insert node at the head (private helper)
+  private insertNodeAtHead(node: ListNode): void {
     node.next = this.head;
     this.head = node;
-  };
+  }
 
-  // Method to create the linked list using given integer array
-  arrayToLinkedList = function (input: number[]) {
+  // Convert array to linked list
+  arrayToLinkedList(input: number[]): void {
     for (let i = input.length - 1; i >= 0; i--) {
       let newNode = new ListNode(input[i]);
       this.insertNodeAtHead(newNode);
     }
-  };
+  }
 
-  // Method that returns the number of nodes in linked list
-  getLength = function (head: ListNode | null): number {
+  // Get the length of the list
+  getLength(): number {
     let length = 0;
-    let current = head;
+    let current = this.head;
     while (current != null) {
       length++;
       current = current.next;
     }
 
     return length;
-  };
+  }
 
-  // Method that returns the node at the specified position(index) of the linked list
-  getNode = function (head: ListNode | null, pos: number): ListNode | null | undefined {
-    if (pos >= this.getLength(head) || pos < 0) {
-      // Return undefined since the index does not exist
-      // Will help the caller method to diagnose the issue
-      return undefined;
+  // Get node at position (0-based index)
+  getNode(pos: number): ListNode | null | undefined {
+    let current = this.head;
+    let counter = 0;
+    while (current != null) {
+      if (counter === pos) {
+        return current;
+      }
+
+      counter++;
+      current = current.next;
     }
 
-    let current = head;
-    for (let i = 0; i < pos; i++) {
-      current = current!.next;
-    }
+    return null; // Position out of bounds
+  }
 
-    return current;
-  };
-
-  // Method to display the elements of the linked list
-  display = function (): string {
+  // Display list as a string
+  display(): string {
     let result = "";
 
     let temp = this.head;
@@ -72,5 +72,5 @@ class LinkedList {
     result += "";
 
     return result;
-  };
+  }
 }
